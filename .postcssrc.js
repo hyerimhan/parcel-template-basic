8550047253ed0922ff215b4c2ae27ab9de4ff30c
Hyerim Han <hyerim_han@naver.com>
2022-10-16 23:07:21 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,23 @@
+// ESM 자바스크립트의 모듈 명칭 -JS
+
+// import
+
+// export
+
+
+
+
+// Common JS -Node.js
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer') 
+
+// export {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
